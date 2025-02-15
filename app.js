9735e30742b91b6caf489898ae19a0ad155c48de
suranjaychandrae <cssuranjaykumar@gmail.com>
2025-02-15 12:23:31 -0800

    frontend

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine" ,"ejs");
+app.set(express.static(path.join(__dirname,"public")));
+app.get("/", function (req, res) {
+    res.send("hey");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
