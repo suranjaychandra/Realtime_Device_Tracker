64dc04bba15b9ca01bbceb5086f8f1ead14718f4
suranjaychandrae <cssuranjaykumar@gmail.com>
2025-02-17 20:32:28 -0800

    ready

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,46 @@
-const socket = io();  // ✅ Now this should work!
+const socket = io(); // ✅ Now this should work!
 
-console.log("✅ script.js loaded!");
+if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            socket.emit("send-location", { latitude, longitude });
+        },
+        (error) => {
+            console.error(error);
+        },
+        {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        }
+    );
+}
 
-socket.on("connect", () => {
-    console.log("✅ Connected to server via Socket.io!");
-});
+// Initialize the map
+const map = L.map("map").setView([0, 0], 16);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "Suranjay in rajkot",
+}).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location" , (data)=>{
+    const{id, latitude,longitude} =data;
+    map.setView([latitude, longitude]);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+
+    }
+    else{
+        markers[id] = L.marker([latitude,longitude]).addTo(map);
+    }
+})
+
+socket.on("user-disconnected",(id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+})
